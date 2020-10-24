12041086d2e665e5e72dd2a370b3212532913652
Asebodi <aditraharditya45@gmail.com>
2020-10-24 13:51:53 +0700

    Changed to load html from js instead of html files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let teamsData
+
 document.addEventListener("DOMContentLoaded", function () {
   // Query selector navigasi
   const elems = document.querySelectorAll(".sidenav");
@@ -7,6 +9,13 @@ document.addEventListener("DOMContentLoaded", function () {
   let page = window.location.hash.substr(1);
   if (page == "") page = "home";
   loadPage(page);
+
+  function loadPage(page) {
+    if (page == "home") loadHome();
+    if (page == "standings") loadStandings();
+    if (page == "teams") loadTeams();
+    if (page == "favorite") loadFav();
+  }
 
   // Konten Navigasi
   function loadNav() {
@@ -35,23 +44,192 @@ document.addEventListener("DOMContentLoaded", function () {
     xhttp.open("GET", "nav.html", true);
     xhttp.send();
   }
-
-  // Konten Halaman
-  function loadPage(page) {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4) {
-        const content = document.querySelector("#body-content");
-        if (this.status == 200) {
-          content.innerHTML = xhttp.responseText;
-        } else if (this.status == 404) {
-          content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
-        } else {
-          content.innerHTML = "<p>Halaman tidak dapat diakses.</p>";
-        }
-      }
-    };
-    xhttp.open("GET", "pages/" + page + ".html", true);
-    xhttp.send();
-  }
 });
+
+function loadHome() {
+  const standings = getStandings();
+  let standingFetch = `
+    <tr>
+      <th>Tim</th>
+      <th class="center-align">Won</th>
+      <th class="center-align">Draw</th>
+      <th class="center-align">Lost</th>
+    </tr>
+  `;
+
+  standings.then((data) => {
+    let html = "";
+
+    for (i = 0; i < 5; i++) {
+      standingFetch += `
+        <tr>
+          <td>${data.standings[0].table[i].team.name}</td>
+          <td class="center-align">${data.standings[0].table[i].won}</td>
+          <td class="center-align">${data.standings[0].table[i].draw}</td>
+          <td class="center-align">${data.standings[0].table[i].lost}</td>
+        </tr>
+      `;
+    }
+
+    html += `
+    <div class="home-wrapper">
+      <div class="home-header">
+        <div class="bg"></div>
+        <div class="container header-content white-text">
+          <div class="header-content-inner">
+            <h2>Bola</h2>
+            <p>Sumber informasi bola Liga Inggris</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <h4 class="center-align">Tim Favorit</h4>
+        <div class="home-fav">
+          <!-- <div class="card no-fav home-fav-item center-align">
+            <h5>Belum ada favorit!</h5>
+          </div> -->
+          <div class="card center-align home-fav-item">
+            <img src="/assets/everton.svg" alt="" />
+            <p class="flow-text">Everton</p>
+          </div>
+
+          <div class="card center-align home-fav-item">
+            <img src="/assets/everton.svg" alt="" />
+            <p class="flow-text">Manchester United</p>
+          </div>
+        </div>
+        <a href="#favorite" class="more right-align">Lebih lanjut..</a>
+
+        <h4 class="center-align">Klasemen</h4>
+        <p class="center-align" style="display: block; margin-top: -10px">
+          Top 5
+        </p>
+        <div class="standing-container">
+          <table class="card standing-table">${standingFetch}</table>
+
+          <a href="#standing" class="more right-align">Lebih lanjut..</a>
+        </div>
+      </div>
+    </div>
+    `;
+
+    document.querySelector("#body-content").innerHTML = html;
+  });
+}
+
+function loadStandings() {
+  const standings = getStandings();
+  let standingFetch = `
+    <tr>
+      <th>Tim</th>
+      <th class="center-align">Won</th>
+      <th class="center-align">Draw</th>
+      <th class="center-align">Lost</th>
+    </tr>
+  `;
+
+  standings.then((data) => {
+    let html = "";
+
+    data.standings[0].table.map((standing) => {
+      standingFetch += `
+        <tr>
+          <td>${standing.team.name}</td>
+          <td class="center-align">${standing.won}</td>
+          <td class="center-align">${standing.draw}</td>
+          <td class="center-align">${standing.lost}</td>
+        </tr>
+      `;
+    });
+
+    html += `
+      <div class="standing-container container">
+        <h3 class="center-align">Klasemen</h3>
+        <p class="center-align" style="display: block; margin-top: -10px">
+          Liga Inggris
+        </p>
+
+        <div class="standing-container">
+          <table class="card standing-table striped">${standingFetch}</table>
+        </div>
+      </div>
+    `;
+
+    document.querySelector("#body-content").innerHTML = html;
+  });
+}
+
+function loadTeams() {
+  const teams = getTeams();
+  let html = "";
+  let teamsFetch = "";
+
+  teams.then((data) => {
+    data.teams.map((team) => {
+      teamsFetch += `
+        <div class="card">
+          <div class="team-title">
+            <img src="${team.crestUrl}" alt="Logo ${team.shortName}" />
+            <strong class="flow-text">${team.name}</strong>
+          </div>
+          <div class="fav-action">
+            <img src="/assets/heart-regular.svg" alt="" />
+          </div>
+        </div>
+      `;
+    });
+
+    html += `
+      <div class="teams-container container">
+        <h3 class="center-align">Tim</h3>
+        <p class="center-align" style="display: block; margin-top: -10px">
+          Liga Inggris
+        </p>
+        ${teamsFetch}
+      </div>
+    `;
+
+    document.querySelector("#body-content").innerHTML = html;
+  });
+}
+
+function loadFav() {
+  const teams = getTeams();
+
+  teams.then(data => {
+    teamsData = data
+
+    
+  })
+
+  let html = `
+    <div class="teams-container container">
+      <h3 class="center-align">Tim Favorit</h3>
+      <p class="center-align" style="display: block; margin-top: -10px">
+        Liga Inggris
+      </p>
+            <div class="card">
+            <div class="team-title">
+              <img src="/assets/everton.svg" alt="" />
+              <strong class="flow-text">$Everton</strong>
+            </div>
+            <div class="fav-action">
+              <img src="/assets/trash.svg" alt="" />
+            </div>
+          </div>
+          <div class="card">
+            <div class="team-title">
+              <img src="/assets/everton.svg" alt="" />
+              <strong class="flow-text">$Everton</strong>
+            </div>
+            <div class="fav-action">
+              <img src="/assets/trash.svg" alt="" />
+            </div>
+          </div>
+    </div>
+  `;
+  let favTeams = ``;
+
+  document.querySelector("#body-content").innerHTML = html;
+}
